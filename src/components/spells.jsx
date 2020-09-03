804a1e08d0e5a04e157f8569c669a50d2d7d20ba
Nelson Mendez <31571728+Nelson-Mendez@users.noreply.github.com>
2020-09-03 12:39:25 -0400

    Added and formatted massive spell list
    Table has sort function

--- src/components/spells.jsx
+++ src/components/spells.jsx
@@ -1,9 +1,55 @@
 import React from 'react';
+import { listofspells } from '../data/spellList';
 
-export default function Spells() {
+export default function SpellList() {
+
+  const [sortedField, setSortedField] = React.useState(null);
+  
+  let sortedSpells = [...listofspells];
+  if (sortedField !== null) {
+    sortedSpells.sort((a, b) => {
+      if (a[sortedField] < b[sortedField]) {
+        return -1;
+      }
+      if (a[sortedField] > b[sortedField]) {
+        return 1;
+      }
+      return 0;
+    });
+  }
+
   return(
-    <div className="spells"> 
-      <h1> Eventually there will be a big ol' table here with a list of all the spells and such</h1>
-    </div>
+    <table> 
+      <caption>Here be a list of spells!</caption>
+
+      <thead>
+        <tr>
+          <th><button type="button" onClick={() => setSortedField('name')}>Name</button></th>
+          <th><button type="button" onClick={() => setSortedField('level')}>Level</button></th>
+          <th><button type="button" onClick={() => setSortedField('school')}>School</button></th>
+          <th><button type="button" onClick={() => setSortedField('time to cast')}>Time to Cast</button></th>
+          <th><button type="button" onClick={() => setSortedField('concentration')}>Concentration</button></th>
+          <th><button type="button" onClick={() => setSortedField('ritual')}>Ritual</button></th>
+          <th><button type="button" onClick={() => setSortedField('description')}>Description</button></th>
+          <th><button type="button" onClick={() => setSortedField('source')}>Source</button></th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {sortedSpells.map(spell => (
+          <tr key={spell.name}>
+            <td>{spell.name}</td>
+            <td>{spell.level}</td>
+            <td>{spell.school}</td>
+            <td>{spell.castTime}</td>
+            <td>{spell.concentration}</td>
+            <td>{spell.ritual}</td>
+            <td>{spell.description}</td>
+            <td>{spell.source}</td>
+          </tr>
+        ))}
+      </tbody>
+
+    </table>
   )
 }
